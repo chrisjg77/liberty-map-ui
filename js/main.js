1796b290923a0078b96f360826d2680300e33368
Chris Madrigal <crismas07@gmail.com>
2014-03-25 18:04:42 -0700

    set up initial layout.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
+    .setView([38, -122], 10);
 
+var $facesList = $('.faces ul li');
+
+$(document).ready(function() {
+  $facesList.height($facesList.width());
+});
+
+$(window).resize(function() {
+  $facesList.height($facesList.width());
+});
+
+$('.sign-here a').click(function(e) {
+  e.preventDefault()
+  $(this).hide();
+  $('.signup-form').show().addClass('animated fadeInDown');
+});
